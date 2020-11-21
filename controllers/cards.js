920251b0a4d81205a24e63a7223b9490b49f44cc
Nolm <nolm88@yandex.ru>
2020-11-21 23:11:09 +0300

    Исправление ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,12 +13,19 @@ const getCards = (req, res) => {
 const createCard = (req, res) => {
   Card.create({ owner: req.user._id, ...req.body })
     .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => res.status(500).send(err));
 };
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params._id)
+    .orFail(new Error('NotValidId'))
     .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(400).send(err));
+    // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.message === 'NotValidId') {
+        return res.status(404).send({ message: 'Карточка не найдена' });
+      }
+      res.status(500).send({ message: 'Ошибка!' });
+    });
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -8,17 +8,22 @@ const getUsers = (req, res) => {
 const getUser = (req, res) => {
   const { _id } = req.params;
   // eslint-disable-next-line consistent-return
-  User.findOne({ _id }).then((user) => {
-    if (!user) {
-      return res.status(404).send({ message: 'Пользователь не найден' });
-    }
-    res.send(user);
-  });
+  User.findOne({ _id })
+    .orFail(() => new Error('NotValidId', 'Пользователь не найден'))
+    .then((user) => res.status(200).send(user))
+  // eslint-disable-next-line consistent-return
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Некорректные данные' });
+      } else if (err.name === 'NotValidId') {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      } else res.status(500).send({ message: 'Ошибка!' });
+    });
 };
 // eslint-disable-next-line max-len
-const createUser = (req, res) => User.countDocuments().then((count) => User.create({ id: count, ...req.body })
+const createUser = (req, res) => User.countDocuments().then((count) => User.create({ count, ...req.body })
   .then((user) => res.status(200).send(user))
-  .catch((err) => res.status(400).send(err)));
+  .catch((err) => res.status(500).send(err)));
 
 module.exports = {
   getUsers,
