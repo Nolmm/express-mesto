ec7494462bab5c8796ce328e5d6b49315c996af6
Nolm <nolm88@yandex.ru>
2020-11-22 21:50:52 +0300

    иссправление ошибок3

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,13 +2,8 @@ const Card = require('../models/card');
 
 const getCards = (req, res) => {
   Card.find()
-    // eslint-disable-next-line consistent-return
-    .then((data) => {
-      if (!data) {
-        return res.status(404).send({ message: 'Данные не найдены' });
-      }
-      res.send(data);
-    });
+    .then((data) => res.send(data))
+    .catch(() => res.status(500).send({ message: 'Ошибка!' }));
 };
 const createCard = (req, res) => {
   Card.create({ owner: req.user._id, ...req.body })
@@ -25,11 +20,10 @@ const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params._id)
     .orFail(new Error('NotValidId', 'Карточка не найдена'))
     .then((card) => res.status(200).send(card))
-    // eslint-disable-next-line consistent-return
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Некорректные данные' });
-      } else if (err.name === 'NotValidId') {
+      } else if (err.message === 'NotValidId') {
         res.status(404).send({ message: 'Карточка не найдена' });
       } else res.status(500).send({ message: 'Ошибка!' });
     });
--- controllers/users.js
+++ controllers/users.js
@@ -7,20 +7,17 @@ const getUsers = (req, res) => {
 };
 const getUser = (req, res) => {
   const { _id } = req.params;
-  // eslint-disable-next-line consistent-return
   User.findOne({ _id })
     .orFail(() => new Error('NotValidId', 'Пользователь не найден'))
     .then((user) => res.status(200).send(user))
-  // eslint-disable-next-line consistent-return
     .catch((err) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Некорректные данные' });
-      } else if (err.name === 'NotValidId') {
+      } else if (err.message === 'NotValidId') {
         res.status(404).send({ message: 'Пользователь не найден' });
       } else res.status(500).send({ message: 'Ошибка!' });
     });
 };
-// eslint-disable-next-line max-len
 const createUser = (req, res) => User.create(req.body)
   .then((user) => res.status(200).send(user))
   .catch((err) => {
